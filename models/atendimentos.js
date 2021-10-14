57ca0f7fe179c9fdcf68b6b9fb8431ba9f243152
Fabricio Oliveira <fabriciooh22@gmai.com>
2021-10-14 18:12:05 -0300

    applying input validations

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -7,18 +7,45 @@ class Atendimento {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         // Formatando a data e hora com o uso da lib moment
         const data =  moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        // Atendimento com a data de criação.
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        
-        const sql = 'INSERT INTO Atendimentos SET ?'
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro)
-            } else {
-                res.status(201).json(resultados)
+        // Tratamento de erros
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data dever ser maior ou igual a data atual',
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
             }
-        })
+        ]
+
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            // Atendimento com a data de criação.
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+        
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }
     }
 }
 
