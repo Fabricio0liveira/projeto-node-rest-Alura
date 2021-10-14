ffeef30d09005860b7244e72dbc83a3e858706a9
Fabricio Oliveira <fabriciooh22@gmai.com>
2021-10-14 08:30:38 -0300

    adding date to service

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,11 +1,18 @@
 // Dentro de "PROJETO", criaremos uma nova pasta chamada "models", a qual terá a conexão com o banco de dados e algumas validações de regra de negócios. Nosso Controller decidirá qual será a requisição que faremos.
-
+const moment =  require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
     adiciona(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        // Formatando a data e hora com o uso da lib moment
+        const data =  moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        // Atendimento com a data de criação.
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
+
         const sql = 'INSERT INTO Atendimentos SET ?'
-        conexao.query(sql, atendimento, (erro, resultados) => {
+
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if(erro) {
                 console.log(erro)
             } else {
